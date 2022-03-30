38aa60f4557b28400bbd76878ce19b1ea2e4239a
Tran Cong Thinh <congthinh6304@gmail.com>
2022-03-31 00:11:44 +0700

    update cover left animation

--- animations.py
+++ animations.py
@@ -1,7 +1,7 @@
 import cv2
 import imageio as io
 
-def cover_animation(img1, img2, filename, color_mode = "RGB", speed = 6, save_mode = True):
+def cover_animation(img1, img2, filename, from_right = True, color_mode = "RGB", speed = 6, save_mode = True):
     
     if color_mode == "BGR":
         img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
@@ -17,12 +17,23 @@ def cover_animation(img1, img2, filename, color_mode = "RGB", speed = 6, save_mo
     img2 = cv2.resize(img2, (w, h))
     
     results = []
-    for D in range(0, w+1, speed):
-        result = img1.copy()
-        result[:,0:w-D,:] = img1[:,D:w,:]
-        result[:,w-D:w,:] = img2[:,0:D,:]
-        # create gif file
-        results.append(result)
+    
+    if from_right:
+        for D in range(0, w + 1, speed):
+            result = img1.copy()
+            result[:,0:w-D,:] = img1[:,D:w,:]
+            result[:,w-D:w,:] = img2[:,0:D,:]
+            # create gif file
+            results.append(result)
+            
+    else:
+        for D in range(0, w + 1, speed):
+            result = img1.copy()
+            
+            result[:,0:D,:] = img1[:,w-D:w,:]
+            result[:,D:w,:] = img2[:,0:w-D]
+
+            results.append(result)
 
     if save_mode:
         io.mimsave(filename, results)
