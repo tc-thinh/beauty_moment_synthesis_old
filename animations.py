b3341397acb8a94c571815332bf51a91b8eea34f
Tran Cong Thinh <congthinh6304@gmail.com>
2022-04-02 21:10:21 +0700

    update animation output

--- animations.py
+++ animations.py
@@ -1,41 +1,46 @@
 import cv2
 import imageio as io
 
-def cover_animation(img1, img2, filename, from_right = True, color_mode = "RGB", speed = 6, save_mode = True):
+def cover_animation(img1, img2, filename, from_right = True, color_mode = "BGR", fps = 30, speed = 1):
     
-    if color_mode == "BGR":
+    if color_mode == "RGB":
         img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
         img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
     
-    h1, w1, c1 = img1.shape
-    h2, w2, c2 = img2.shape
- 
+    h1, w1, c1 = image1.shape
+    h2, w2, c2 = image2.shape
+    
     h = min(h1, h2)
     w = min(w1, w1)
-
-    img1 = cv2.resize(img1, (w, h))
-    img2 = cv2.resize(img2, (w, h))
+    
+    frameSize = (w, h)
+    out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'DIVX'), fps, frameSize)    
+    
+    img1 = cv2.resize(image1, (w,h))
+    img2 = cv2.resize(image2, (w,h))
     
     results = []
     
     if from_right:
-        for D in range(0, w + 1, speed):
+        for D in range(0, w+1, speed):
+            
             result = img1.copy()
+            
             result[:,0:w-D,:] = img1[:,D:w,:]
             result[:,w-D:w,:] = img2[:,0:D,:]
-            # create gif file
-            results.append(result)
+            
+            out.write(result)
             
     else:
-        for D in range(0, w + 1, speed):
+        for D in range(0, w+1, speed):
             result = img1.copy()
             
             result[:,0:D,:] = img1[:,w-D:w,:]
             result[:,D:w,:] = img2[:,0:w-D]
 
-            results.append(result)
-
-    if save_mode:
-        io.mimsave(filename, results)
-    else:
-        return results
+            out.write(result)
+            
+    # io.mimsave(filename, results, fps = 60)
+    out.release()
+    
+# cover_animation(img1, img2, filename = "results/output_video.avi", from_right = False, color_mode = "BGR", fps = 256, speed = 1) 
