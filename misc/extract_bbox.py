dd16a3abe26f3e24d379c903fbef8979c04091b6
TranCongThinh <congthinh6304@gmail.com>
2022-04-16 23:05:43 +0700

    update bbox format

--- misc/extract_bbox.py
+++ misc/extract_bbox.py
@@ -1,10 +1,94 @@
 import cv2
 
 
+def convert_bounding_box(box, input_type, change_to):
+    """
+    This function converts an input bounding box to either YOLO, COCO, or OpenCV
+    format.
+    However, the function only converts the input bounding box if it already belongs
+    to one of the three formats listed above.
+    Note:
+    + OpenCV-formatted bounding box has 4 elements [x_left, y_top, x_right, y_bot]
+    + YOLO-formatted bounding box has 4 elements [x_center, y_center, width, height]
+    + COCO-formatted bounding box has 4 elements [x_left, y_top, width, height]
+    Parameters
+    ----------
+    + box : list.
+        The provided bounding box in the Python list format. The given bounding box
+        must have 4 elements, corresponding to its format.
+    + input_type : {'opencv', 'yolo', 'coco'}
+        The format of the input bounding box.
+        Supported values are 'yolo' - for YOLO format, 'coco' - for COCO format,
+        and 'opencv' - for OpenCV format.
+    + change_to : {'opencv', 'yolo', 'coco'}.
+        The type of format to convert the input bounding box to.
+        Supported values are 'yolo' - for YOLO format, 'coco' - for COCO format,
+        and 'opencv' - for OpenCV format.
+    Return
+    ----------
+        Returns a list for the converted bounding box.
+    """
+    assert (type(box) == list), 'The provided bounding box must be a Python list'
+    assert (len(box) == 4), 'Must be a bounding box that has 4 elements: [x_left, y_top, x_right, y_bot] (OpenCV format)'
+    assert (input_type == 'yolo' or input_type == 'coco' or input_type == 'opencv'), "Must select either 'yolo', 'coco', or 'opencv' as a format of your input bounding box"
+    assert (change_to == 'yolo' or change_to == 'coco' or change_to == 'opencv'), "Must select either 'yolo', 'coco', or 'opencv' as a format you want to convert the input bounding box to"
+    assert (input_type != change_to), "The format of your input bounding box must be different from your output bounding box."
+
+    if input_type == 'opencv':
+        x_left, y_top, x_right, y_bot = box[0], box[1], box[2], box[3]
+
+        if change_to == 'yolo':
+            x_center = int((x_left + x_right) / 2)
+            y_center = int((y_top + y_bot) / 2)
+            width = int(x_right - x_left)
+            height = int(y_bot - y_top)
+
+            return [x_center, y_center, width, height]
+
+        elif change_to == 'coco':
+            width = int(x_right - x_left)
+            height = int(y_bot - y_top)
+
+            return [x_left, y_top, width, height]
+
+    elif input_type == 'yolo':
+        x_center, y_center, width, height = box[0], box[1], box[2], box[3]
+
+        if change_to == 'opencv':
+            x_left = int(x_center - width / 2)
+            x_right = int(x_center + width / 2)
+            y_top = int(y_center - height / 2)
+            y_bot = int(y_center + height / 2)
+
+            return [x_left, x_right, y_top, y_bot]
+
+        elif change_to == 'coco':
+            x_left = int(x_center - width / 2)
+            y_top = int(y_center - height / 2)
+
+            return [x_left, y_top, width, height]
+
+    elif input_type == 'coco':
+        x_left, y_top, width, height = box[0], box[1], box[2], box[3]
+
+        if change_to == 'opencv':
+            x_right = int(x_left + width)
+            y_bot = int(y_top + height)
+
+            return [x_left, x_right, y_top, y_bot]
+
+        elif change_to == 'yolo':
+            x_center = int(x_left + width / 2)
+            y_center = int(y_top + height / 2)
+
+            return [x_center, y_center, width, height]
+        
+        
 def get_target_bbox(img_path, bboxes, p=0.1):
     img = cv2.imread(img_path)
     data = []
     for bbox in bboxes:
+        bbox = convert_bounding_box(box=bbox, input_type="opencv", change_to="coco")
         x, y = int(bbox[0]), int(bbox[1])  # top-left x, y corrdinates
         w, h = int(bbox[2]), int(bbox[3])  # w, h values
 
